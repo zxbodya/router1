c921bba60a20baf85868ed188e2ffc11a47907ca
Bogdan Savluk <savluk.bogdan@gmail.com>
2015-11-26 13:10:18 +0200

    add location source to be able to implement correct scroll behaviour

--- src/createBrowserHistory.js
+++ src/createBrowserHistory.js
@@ -1,17 +1,18 @@
 import {Observable, Subject} from 'rx';
 
 function historyFactory() {
-  function currentLocation() {
+  function currentLocation(source) {
     return {
       pathname: window.location.pathname,
       search: window.location.search,
       hash: window.location.hash,
+      source,
     };
   }
 
   const changes = new Subject();
-  const next = ()=> {
-    changes.onNext(currentLocation());
+  const next = (source)=> {
+    changes.onNext(currentLocation(source));
   };
 
   let location;
@@ -21,30 +22,30 @@ function historyFactory() {
   if ('onpopstate' in window) {
     location = Observable
       .fromEvent(window, 'popstate')
-      .map(()=>currentLocation())
-      .startWith(currentLocation())
+      .map(()=>currentLocation('pop'))
+      .startWith(currentLocation('init'))
       .merge(changes)
       .shareReplay();
 
     push = (url, data = null, title = null)=> {
       window.history.pushState(data, title, url);
-      next();
+      next('push');
     };
     replace = (url, data = null, title = null)=> {
       window.history.replaceState(data, title, url);
-      next();
+      next('replace');
     };
   } else {
     replace = (url)=> {
       window.location.replace(url);
-      next();
+      next('push');
     };
     push = (url)=> {
       window.location.assign(url);
-      next();
+      next('replace');
     };
     location = Observable
-      .return(currentLocation())
+      .return(currentLocation('init'))
       .merge(changes)
       .shareReplay();
   }
