0c8baee65cd10c829ca2f2ad2334a6d8794e134a
Bogdan Savluk <savluk.bogdan@gmail.com>
2016-06-26 22:23:21 +0300

    use test history, in router tests, test case when there is no active route, test case when route params are cheched in router.isActive

--- src/Router.test.js
+++ src/Router.test.js
@@ -1,10 +1,10 @@
 import { Router } from './Router';
 import { Observable } from 'rx';
-import { createServerHistory } from './createServerHistory';
+import { createTestHistory } from './createTestHistory';
 
 describe('Router', () => {
   it('works with empty route collection', (done) => {
-    const history = createServerHistory('/');
+    const history = createTestHistory('/');
     const router = new Router({
       routes: [],
       history,
@@ -12,12 +12,16 @@ describe('Router', () => {
     });
 
     expect(router.createUrl('main', {}, '')).toEqual('#route-main-not-found');
+    expect(router.isActive('main')).toEqual(false);
 
-    router.renderResult().subscribe(renderResult => {
+    router.renderResult().first().subscribe(renderResult => {
       expect(renderResult.route).toEqual(null);
       expect(renderResult.params).toEqual({});
       expect(renderResult.handlers).toEqual([]);
       expect(renderResult.location).toEqual({ pathname: '/', search: '', hash: '', state: {} });
+
+      expect(router.isActive('main')).toEqual(false);
+      expect(router.activeRoute).toEqual([null, {}]);
     }, () => {
     }, () => {
       done();
@@ -25,7 +29,7 @@ describe('Router', () => {
   });
 
   it('matches route', (done) => {
-    const history = createServerHistory('/');
+    const history = createTestHistory('/');
     const router = new Router({
       routes: [{
         name: 'main',
@@ -39,7 +43,7 @@ describe('Router', () => {
     expect(router.createUrl('main', {}, '')).toEqual('/');
     expect(router.createUrl('main2', {}, '')).toEqual('#route-main2-not-found');
 
-    router.renderResult().subscribe(renderResult => {
+    router.renderResult().first().subscribe(renderResult => {
       expect(renderResult.route).toEqual('main');
       expect(renderResult.params).toEqual({});
       expect(renderResult.handlers).toEqual(['main']);
@@ -52,7 +56,7 @@ describe('Router', () => {
   });
 
   it('matches route with params', (done) => {
-    const history = createServerHistory('/123');
+    const history = createTestHistory('/123');
     const router = new Router({
       routes: [{
         name: 'main',
@@ -65,7 +69,7 @@ describe('Router', () => {
 
     expect(router.createUrl('main', { page: 1000 }, '')).toEqual('/1000');
 
-    router.renderResult().subscribe(renderResult => {
+    router.renderResult().first().subscribe(renderResult => {
       expect(renderResult.route).toEqual('main');
       expect(renderResult.params).toEqual({ page: '123' });
       expect(renderResult.handlers).toEqual(['main']);
@@ -73,12 +77,14 @@ describe('Router', () => {
     }, () => {
     }, () => {
       expect(router.isActive('main')).toEqual(true);
+      expect(router.isActive('main', { page: '123' })).toEqual(true);
+      expect(router.isActive('main', { page: '321' })).toEqual(false);
       done();
     });
   });
 
   it('matches route with params and search query and hash', (done) => {
-    const history = createServerHistory('/123?q=text#anchor');
+    const history = createTestHistory('/123?q=text#anchor');
     const router = new Router({
       routes: [{
         name: 'main',
@@ -92,7 +98,7 @@ describe('Router', () => {
     expect(router.createUrl('main', { page: 1000, q: 1234 }, '')).toEqual('/1000?q=1234');
     expect(router.createUrl('main', { page: 1000, q: 1234 }, 'anchor')).toEqual('/1000?q=1234#anchor');
 
-    router.renderResult().subscribe(renderResult => {
+    router.renderResult().first().subscribe(renderResult => {
       expect(renderResult.route).toEqual('main');
       expect(renderResult.params).toEqual({ page: '123', q: 'text' });
       expect(renderResult.handlers).toEqual(['main']);
@@ -100,6 +106,10 @@ describe('Router', () => {
     }, () => {
     }, () => {
       expect(router.isActive('main')).toEqual(true);
+      expect(router.isActive('main', { page: '123', q: 'text' })).toEqual(true);
+      expect(router.isActive('main', { page: '123', q: 'text' })).toEqual(true);
+      expect(router.isActive('main', { page: '123', q: 'tex' })).toEqual(false);
+      expect(router.isActive('main', { page: '12', q: 'text' })).toEqual(false);
       done();
     });
   });
--- src/createTestHistory.js
+++ src/createTestHistory.js
@@ -2,19 +2,18 @@ import { Subject } from 'rx';
 import { locationFromUrl } from './utils/locationFromUrl';
 
 export function createTestHistory(initialUrl, cb) {
-
   const location$ = new Subject();
   return {
     push(url, state = null, title = null) {
-      cb('push', { url, state, title });
+      if (cb) cb('push', { url, state, title });
     },
     replace(url, state = null, title = null) {
-      cb('replace', { url, state, title });
+      if (cb) cb('replace', { url, state, title });
     },
     location: location$
       .startWith(
         locationFromUrl(initialUrl)
       )
-      .shareReplay(1),
+      .shareReplay(),
   };
 }
